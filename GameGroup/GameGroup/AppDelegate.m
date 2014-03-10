c1a7e6e3ff26eb692f01f8a5cb833fc723eccaed
wangxr <wangxr1990123@126.com>
2014-03-10 14:49:59 +0800

    发现手机通讯录

--- GameGroup/GameGroup/AppDelegate.m
+++ GameGroup/GameGroup/AppDelegate.m
@@ -15,7 +15,6 @@
 #import "GetDataAfterManager.h"
 #import "BaseViewController.h"
 
-#import "AddAddressBookViewController.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -23,12 +22,6 @@
 //    [NSThread sleepForTimeInterval:3.0];//开机图停留秒数
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    AddAddressBookViewController * aabVC = [[AddAddressBookViewController alloc]init];
-    UINavigationController * nav = [[UINavigationController alloc]initWithRootViewController:aabVC];
-    self.window.rootViewController = nav;
-    [self.window makeKeyAndVisible];
-    
-    return YES;
     // Override point for customization after application launch.
     self.startViewController = [[StartViewController alloc] init];
     
